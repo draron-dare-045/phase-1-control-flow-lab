ff0c3a035eed7e54000bbc443918082c4c465466
draron-dare-045 <aronondieki045@gmail.com>
2025-03-18 13:25:16 +0300

    9 passing

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  
+  if(ride <= 400){
+    return('This one is on me!')
+  }
+    if( ride <=2000 ){
+    return('That will be twenty bucks.')
+  }
+   if( ride <=2500){
+    return('I will gladly take your thirty bucks.') 
+  }
+  if(ride > 2500){
+    return('No can do.')
+  }
+(scuberGreetingForFeet(199))
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ?  "Ok, sounds good." : "No go.";
 }
+(ternaryCheckCity('Nairobi'));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charmFromTip){
+// Write your code here!
+  switch(charmFromTip){
+    case "generous" :
+      return charmFromTip = 'Thank you so much.'
+    break;
+    case "not as generous" :
+      return charmFromTip = 'Thank you.'
+    break;
+    case "thanks for everything" :
+     return charmFromTip = 'Bye.'
+    break
+  }
 }
